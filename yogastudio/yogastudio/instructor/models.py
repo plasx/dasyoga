2e9d69bd826ca37eeec656e98688a3b0c2cb3e86
Daniel Rivera <plasx@live.com>
2019-10-12 23:02:35 -0400

    built up the instructor model

--- yogastudio/yogastudio/instructor/models.py
+++ yogastudio/yogastudio/instructor/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Classes(models.Model):
+    class = models.CharField(max_length=20)
+
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=32)
+    bio = models.TextField()
+    photo = models.FilePathField(path="/img")
+    hometown = models.CharField(max_length=32)
+    philosophy = models.TextField()
+    classes = models.ManyToManyField('Classes')
+    experience = models.TextField()
+    certifications = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
